ac940db84f9d06ffd2a8f2335770dcb9a938315b
Milan960 <milan.sachani.ms@gmail.com>
2022-03-13 09:28:07 +0100

    style added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
